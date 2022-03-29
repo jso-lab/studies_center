5e1595a1d8c2f5260ddef94dd71dddc39d7024cf
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-03-29 16:12:19 +0200

    resize images

--- public/index.php
+++ public/index.php
@@ -1,27 +1,10 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
+
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-
-$request = Request::createFromGlobals();
-
-
-Request::setTrustedProxies(
-    ['127.0.0.1', '10.0.0.0/8'],
-    Request::HEADER_X_FORWARDED_AWS_ELB
-);
-
-$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
-$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-if($_SERVER['APP_ENV'] == 'prod') {
-    $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
-}
-if($trustedProxies) {
-    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
-};
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
