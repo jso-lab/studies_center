d6703f47faac549100007dee3060243bc597ec89
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-04-04 10:31:39 +0200

    update

--- src/Controller/Student/StudentCrudController.php
+++ src/Controller/Student/StudentCrudController.php
@@ -4,15 +4,11 @@ namespace App\Controller\Student;
 
 use App\Entity\Student;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class StudentCrudController extends AbstractCrudController
 {
-
-    public const STUDENTS_BASE_PATH = 'images/users';
-    public const STUDENTS_UPLOAD_DIR = 'public/images/users';
 
     public static function getEntityFqcn(): string
     {
@@ -25,13 +21,10 @@ class StudentCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            TextField::new('firstName', 'Nom'),
-            TextField::new('lastName', 'Prénom'),
+            TextField::new('pseudo', 'Pseudo'),
             TextField::new('email', 'Adresse mail'),
             TextField::new('password', 'Mot de passe'),
-            ImageField::new('profilPicture', 'Photo de profil')
-                ->setBasePath(self::STUDENTS_BASE_PATH)
-                ->setUploadDir(self::STUDENTS_UPLOAD_DIR)
+           
         ];
     }
 }
