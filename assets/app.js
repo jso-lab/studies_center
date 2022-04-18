04de769f1ae57f4add2bb31561c5ce4a60591421
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-04-18 15:46:23 +0200

    set data for teachers admin

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,10 @@
 import './styles/app.scss';
 
 import './bootstrap';
+
+const $ = require('jquery');
+require('bootstrap');
+
+$(document).ready(function () {
+	$('[data-toggle="popover"]').popover();
+});
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,6 +22,11 @@ class User  implements  UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', nullable: true, length: 255)]
     private $pseudo;
 
+   
+    private $firstName;
+
+    private $lastName;
+
     #[ORM\Column(type: 'string', nullable: true, length: 255)]
     private $email;
 
@@ -65,6 +70,29 @@ class User  implements  UserInterface, PasswordAuthenticatedUserInterface
     public function setPseudo(string $pseudo): self
     {
         $this->pseudo = $pseudo;
+
+        return $this;
+    }
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
