12583e389247de5e19d30c40503f161f9581c048
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-04-19 15:21:55 +0200

    crsf protection enable

--- src/Form/LessonType.php
+++ src/Form/LessonType.php
@@ -2,10 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
+
 use App\Entity\Lesson;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,6 +18,13 @@ class LessonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+        ->add('name', RangeType::class, [
+            'mapped' => false,
+            'attr' => [
+                'min' => 5,
+                'max' => 50
+            ],
+        ])
             ->add('title', TextType::class, [
                 'label' => 'intitulé'
             ])
